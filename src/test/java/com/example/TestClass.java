b3bc5f066c3f73a1c3a503b1e373649fc5a0ead0
thomas yang <ywjno.dev@gmail.com>
2024-12-23 23:19:19 +0900

    first commit

---
+++ src/test/java/com/example/TestClass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class TestClass {
+    public String testMethod() {
+        String str = "test";
+        str.toString();
+        Arrays.asList(str);
+        return str.toString();
+    }
+}
